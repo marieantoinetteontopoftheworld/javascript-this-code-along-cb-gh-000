f27590a36e47fb8c91521f2ff9172589b9dd06dd
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 17:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,16 @@ function serve() {
 
 serve.call(gc);
 serve.apply(pbj, ["Terry", "Tom", "Tabitha"]);
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+pbj.describe();
