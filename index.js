2618359ef774759053ef596bb0938d06ddb1c098
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 17:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const app = "I don't do much."
 
 function serve() {
-    if(arguments.length > 0) {
-        var customers = Array.prototype.slice.call(arguments);
-        last = customers.pop();
-        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
-    } else {
-        console.log(this.name + ". Order up!");
-    }
+  if(arguments.length > 0) {
+      var customers = Array.prototype.slice.call(arguments);
+      last = customers.pop();
+      console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+  } else {
+      console.log(this.name + ". Order up!");
+  }
+}
 
 
 serve.call(gc);
