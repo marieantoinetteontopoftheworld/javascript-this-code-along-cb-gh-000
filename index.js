2c54505443c6e865730eac71571178aa3f278d7c
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 17:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,3 +43,17 @@ var salad = {
 
 salad.describe = pbj.describe.bind(salad);
 salad.describe();
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+var sally = new Customer('Sally', '4');
+var visitSally = visitTable.bind(sally);
+
+setTimeout(visitSally(), 2000);
