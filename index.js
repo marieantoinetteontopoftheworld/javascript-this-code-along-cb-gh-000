0ade0b024bce75c5f95a00618a5441d450e2dd54
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 17:07:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const app = "I don't do much."
 
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
+
 function serve() {
   if(arguments.length > 0) {
       var customers = Array.prototype.slice.call(arguments);
