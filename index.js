f428d849b9d3d39efbca6cc1bde5fc8c53a77e21
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 17:06:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    } else {
+        console.log(this.name + ". Order up!");
+    }
+
+
+serve.call(gc);
+serve.apply(pbj, ["Terry", "Tom", "Tabitha"]);
